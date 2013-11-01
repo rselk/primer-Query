6b638626a3f6141abfcd4c0ff51f084b6c7752e8
Ryan Selk <ryanselk@gmail.com>
2013-10-31 20:52:26 -0600

    add OrQuery

---
+++ OrQuery.h
@@ -0,0 +1,19 @@
+#ifndef ORQUERY_H
+#define ORQUERY_H
+
+class OrQuery: public BinaryQuery
+{
+  friend Query operator|(const Query&, const Query&);
+
+  private:
+    OrQuery(const Query &left, const Query &right):
+      BinaryQuery(left, right, "|") {}
+    QueryResult eval(const TextQuery&) const;
+};
+
+inline Query operator|(const Query &lhs, const Query &rhs)
+{
+  return std::shared_ptr<QueryBase>(new OrQuery(lsh, rhs));
+}
+
+#endif

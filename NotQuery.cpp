6336274dffb61297294ba4d5e41016d8ff5030dd
Ryan Selk <ryanselk@gmail.com>
2013-10-31 23:29:40 -0600

    update main

--- NotQuery.cpp
+++ NotQuery.cpp
@@ -4,9 +4,7 @@ QueryResult NotQuery::eval(const TextQuery &text) const
 {
   auto result = query.eval(text);
   auto ret_lines = std::make_shared<std::set<line_no>>();
-
   auto beg = result.begin(), end = result.end();
-
   auto sz = result.get_file()->size();
 
   for (size_t n = 0; n!=sz; ++n)
--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include "TextQuery.h"
 #include "QueryResult.h"
 #include "RunQuery.h"
+#include "Query.h"
 
 int main()
 {
   std::ifstream file("text.txt");
-  runQueries(file);
+//  runQueries(file);
+  Query q = Query("fiery") & Query("bird") | Query("wind");
+
 }
